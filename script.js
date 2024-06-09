aee2449bc7509d1031c8b21f0cef627f033fa526
friizor <mohamedzaidi768@gmail.com>
2024-06-09 23:48:58 +0100

    finishing the project

--- script.js
+++ script.js
@@ -34,3 +34,65 @@ function showTodos() {
     emptyImage.style.display = 'none';
   }
 }
+
+function addTodo(todo)  {
+  input.value = "";
+  todosJson.unshift({ name: todo, status: "pending" });
+  localStorage.setItem("todos", JSON.stringify(todosJson));
+  showTodos();
+}
+
+input.addEventListener("keyup", e => {
+  let todo = input.value.trim();
+  if (!todo || e.key != "Enter") {
+    return;
+  }
+  addTodo(todo);
+});
+
+addButton.addEventListener("click", () => {
+  let todo = input.value.trim();
+  if (!todo) {
+    return;
+  }
+  addTodo(todo);
+});
+
+function updateStatus(todo) {
+  let todoName = todo.parentElement.lastElementChild;
+  if (todo.checked) {
+    todoName.classList.add("checked");
+    todosJson[todo.id].status = "completed";
+  } else {
+    todoName.classList.remove("checked");
+    todosJson[todo.id].status = "pending";
+  }
+  localStorage.setItem("todos", JSON.stringify(todosJson));
+}
+
+function remove(todo) {
+  const index = todo.dataset.index;
+  todosJson.splice(index, 1);
+  showTodos();
+  localStorage.setItem("todos", JSON.stringify(todosJson));
+}
+
+filters.forEach(function (el) {
+  el.addEventListener("click", (e) => {
+    if (el.classList.contains('active')) {
+      el.classList.remove('active');
+      filter = '';
+    } else {
+      filters.forEach(tag => tag.classList.remove('active'));
+      el.classList.add('active');
+      filter = e.target.dataset.filter;
+    }
+    showTodos();
+  });
+});
+
+deleteAllButton.addEventListener("click", () => {
+  todosJson = [];
+  localStorage.setItem("todos", JSON.stringify(todosJson));
+  showTodos();
+});
